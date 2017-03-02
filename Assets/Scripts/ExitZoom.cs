ddbb61403e23c142cd8843b4475d862fd0054357
TimidJay <justizl1@uci.edu>
2017-03-01 22:13:11 -0800

    Exit boundaries work for dollhouse now!
    Slight delay when loading back to perfect room

---
+++ Assets/Scripts/ExitZoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitZoom : MonoBehaviour {
+    public string destination;
+    private GameObject menu;
+    // Use this for initialization
+    private void Start()
+    {
+        menu = GameObject.Find("MenuC");
+    }
+    private void OnMouseDown()
+    {
+        print("true");
+        menu.GetComponent<MenuCTRL>().LoadScene(destination);
+    }
+}
